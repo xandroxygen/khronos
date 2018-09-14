7d117512b0ced19c6668c99544795a100a15027e
Xander Moffatt <xmoffatt@instructure.com>
2018-09-14 17:02:02 -0600

    change status to whatever users dm me

--- api.js
+++ api.js
@@ -9,6 +9,22 @@ const postMessage = async data => {
   return await slackAPI.post("chat.postMessage", data);
 };
 
+const changeStatus = async ({
+  user,
+  status_emoji,
+  status_text,
+  status_expiration = 0
+}) => {
+  return await slackAPI.post(
+    "users.profile.set",
+    {
+      profile: { status_emoji, status_text, status_expiration }
+    },
+    { headers: { "X-Slack-User": user } }
+  );
+};
+
 module.exports = {
-  postMessage
+  postMessage,
+  changeStatus
 };
